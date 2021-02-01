42afa8a57ed8df9596f4d636f527bd4fa39c4722
Subhash Chaudhary <myearning4me@gmail.com>
2021-02-01 09:31:24 +0530

    Auth Methods Added

--- src/action/auth.js
+++ src/action/auth.js
@@ -4,7 +4,7 @@ import database from "@react-native-firebase/database"
 
 
 
-export const signUp = (data) => async () => {
+export const signUp = (data) => async (dispatch) => {
     console.log(data);
     const {name, instaUserName, bio, email, password, country, image} = data
 
@@ -12,6 +12,24 @@ export const signUp = (data) => async () => {
     .then((data)=>{
         console.log(data)
         console.log("User Created successfully")
+        database().ref("/users/" + data.user.uid)
+        .set(
+            name,
+            instaUserName,
+            country,
+            image,
+            bio,
+            uid = data.user.uid
+        )
+        .then(()=>console.log("data set success"))
+        Snackbar.show({
+            text: "account created",
+            textColor: "white",
+            backgroundColor: "#1b262c"
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
     })
     .catch((error)=>{
         console.error((error)=>{
@@ -23,4 +41,60 @@ export const signUp = (data) => async () => {
             })
         })
     })
+}
+
+export const signIn = (data) => async (dispatch) => {
+    console.log(data)
+    const {email, password} = data
+    auth()
+    .signInWithEmailAndPassword(email,password)
+    .then(
+        ()=>{
+            console.log("SignIn Success")
+            Snackbar.show({
+                text: "Account signin",
+                textColor:"white",
+                backgroundColor:"#1b262c"
+            })
+        }
+    )
+    .catch(
+        (error) => {
+            console.log(error)
+            Snackbar.show(
+                {
+                    text: "Something went wrong",
+                    textColor: "white",
+                    backgroundColor: "red"
+                }
+            )
+        
+        }
+    )
+
+    
+}
+
+export const signOut = () => async(dispatch)=> {
+    auth()
+    .signOut()
+    .then(
+        ()=>{
+            Snackbar.show({
+                text: "Signout success",
+                textColor: "white",
+                backgroundColor:"#1b262c"
+            })
+        }
+    )
+    .catch(
+        (error)=>{
+            console.log(error)
+            Snackbar.show({
+                text: "Signout failed",
+                textColor: "white",
+                backgroundColor:"red"
+            })
+        }
+    )
 }
