1620822c5e8cda591c6f2b4fd909200fe37c0beb
Subhash Chaudhary <myearning4me@gmail.com>
2021-02-02 09:38:27 +0530

    LAYOUT ADDED

--- src/components/EmptyContainer.js
+++ src/components/EmptyContainer.js
@@ -1,14 +1,26 @@
 import  React from "react";
-
 import { StyleSheet, Text } from "react-native";
+import { Container, Spinner } from "native-base";
 
 
-const AddPost = () => {
+const EmptyContainer = () => {
     return(
-        <>
-        <Text>Hello from AddPost</Text>
-        </>
+        <Container style={styles.emptyContainer}>
+            <Spinner/>
+        </Container>
     )
+    
 }
 
-export default AddPost;
+export default EmptyContainer;
+
+const styles = StyleSheet.create(
+    {
+        emptyContainer:{
+            flex: 1,
+            backgroundColor:"#1b262c",
+            justifyContent:"center",
+            alignItems:"center"
+        }
+    }
+)
