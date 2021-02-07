3c63287093fe8bc4e17f919fc933ed9328dd4f73
Subhash Chaudhary <myearning4me@gmail.com>
2021-02-07 22:45:06 +0530

    Custom Header Updated

--- layout/CustomHeader.js
+++ layout/CustomHeader.js
@@ -1,13 +1,78 @@
 import  React from "react";
 
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
 
-const CustomHeader = () => {
+import { 
+    Body,
+    Header,
+    Right,
+    Button,
+    Icon,
+    Title,
+    Text
+ } from "native-base"
+
+ import { connect } from "react-redux";
+
+ import propTypes from "prop-types";
+
+ import { signOut } from "../src/action/auth";
+import { State } from "react-native-gesture-handler";
+
+
+const CustomHeader = ({signOut, authState, navigation}) => {
     return(
-        <>
-        <Text>Hello from CustomHeader</Text>
-        </>
+        <Header
+        androidStatusBarColor="#0f4c75"
+        style={{
+            backgroundColor: "#0f4c75"
+        }}
+        >
+            <Body>
+                <Title>
+                    Social App LCO
+                </Title>
+            </Body>
+            <Right>
+                {authState.isAuthenticated && (
+                    <>
+                    <Button
+                    transparent
+                    iconLeft
+                    onPress={()=>navigation.navigate("AddPost")}
+                    >
+                        <Text
+                        style={{color:"#fdcb9e"}}
+                        >
+                            Add Post
+                        </Text>
+                    </Button>
+                    <Button
+                    transparent
+                    onPress={()=>signOut()}
+                    >
+                        <Text>
+                            SignOut
+                        </Text>
+                    </Button>
+                    </>
+                )}
+            </Right>
+        </Header>
     )
 }
 
-export default CustomHeader;
+const mapStateToProps = (state) =>({
+    authState: state.auth
+})
+
+const mapDispatchToProps = {
+    signOut
+}
+
+CustomHeader.propTypes = {
+    signOut: propTypes.func.isRequired,
+    authState: propTypes.object.isRequired
+} 
+
+export default connect(mapStateToProps, mapDispatchToProps)(CustomHeader);
