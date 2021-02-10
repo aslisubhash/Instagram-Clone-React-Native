68198ca00ed27bf83dc110e470ae0d2665e96b98
Subhash Chaudhary <myearning4me@gmail.com>
2021-02-10 08:38:20 +0530

    Signup updated

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,8 +1,32 @@
-import  React from "react";
+import  React, {useState} from "react";
 
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, ScrollView, TouchableOpacity, View } from "react-native";
+import {
+    Text,
+    Container,
+    Form,
+    Item,
+    Input,
+    Button,
+    Thumbnail,
+    Content
+} from "native-base"
 
-const signUp = () => {
+import storage from "@react-native-firebase/storage"
+import ProgressBar from "react-native-progress/Bar";
+import ImagePicker from "react-native-image-picker";
+import {options} from "../utils/options"
+
+import PropTypes from "prop-types";
+import { signUp } from "../action/auth";
+import { connect } from "react-redux";
+
+const signUp = ({signUp}) => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [instaUsername, setInstaUsername] = useState("")
+
     return(
         <>
         <Text>Hello from SignUP</Text>
@@ -10,4 +34,12 @@ const signUp = () => {
     )
 }
 
-export default signUp;
+const mapDispatchToProps = {
+    signUp: (data) => signUp(data)
+}
+
+signUp.PropTypes = {
+    signUp: PropTypes.func.isRequired
+}
+
+export default connect(null, mapDispatchToProps)(signUp)
--- src/utils/AskPermission.js
+++ src/utils/AskPermission.js
@@ -4,7 +4,7 @@ import { PermissionsAndroid, ToastAndroid } from "react-native";
 
 export const requestPermission =  async () => {
     try {
-        const granted = await PermissionsAndroid.request([
+        const granted = await PermissionsAndroid.requestMultiple([
             PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
             PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
         ])
